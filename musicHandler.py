6808c257e48007290f0c5f072126fb9dae502502
Dylan Garbanzo <dgarbanzof@estudiantec.cr>
2023-10-01 20:59:35 -0600

    feat: music, questions and biometrics
    music, questions and biometrics >
    
    implementation of getting and saving 3 music downloads, 5 security quiestions,

--- musicHandler.py
+++ musicHandler.py
@@ -1,6 +1,11 @@
 import tkinter as tk
 from pytube import YouTube
 import os
+import cv2
+from matplotlib import pyplot
+from mtcnn.mtcnn import MTCNN
+import numpy as np
+import tensorflow
 import requests
 from googletrans import Translator
 
@@ -12,6 +17,7 @@ suggestions = []
 
 songCount = 0
 targetLanguage = 'es'
+boolImage = True
 
 
 def search_youtube(query):
@@ -32,6 +38,47 @@ def search_youtube(query):
     except Exception as e:
         print("Error:", e)
         return None
+
+
+def saveInformation():
+    with open("Data/tempUser.txt", "r") as tempFile:
+        textUsername = tempFile.readline()
+
+    mainDir = f"Data/{textUsername}/"
+    os.makedirs(os.path.dirname(f"{mainDir}Images/"), exist_ok=True)
+    file_path = "Data/" + textUsername + "/information.txt"
+    with open(file_path, "r") as file1:
+        first = file1.readline()
+        second = file1.readline()
+    with open(file_path, "w") as file:
+        file.write(first)
+        file.write(second)
+        file.write(q1.get() + "\n")
+        file.write(q2.get() + "\n")
+        file.write(q3.get() + "\n")
+        file.write(q4.get() + "\n")
+        file.write(q5.get() + "\n")
+
+
+def on_click(event):
+    # Define the region coordinates and dimensions
+    x1, y1, x2, y2 = 870, 970, 1066, 1039
+
+    # Get the click coordinates
+    click_x, click_y = event.x, event.y
+
+    x11, y11, x22, y22 = 281, 835, 581, 863
+
+    # Check if the click falls within the specified region
+    if x1 <= click_x <= x2 and y1 <= click_y <= y2:
+        if q1.get() and q2.get() and q3.get() and q4.get() and q5.get() != "":
+            saveInformation()
+            if song1.cget("text") and song2.cget("text") and song3.cget("text") != "":
+                if boolImage:
+                    print("Click detected within the specified region!")
+
+    if x11 <= click_x <= x22 and y11 <= click_y <= y22:
+        facialRegister()
 
 
 def update_suggestions():
@@ -103,15 +150,59 @@ def downloadYtVideo(yt, selected_title):
     audio_stream.download(output_path=output_path, filename=f"{selected_title}.mp3")
 
 
+def facialRegister():
+    with open("Data/tempUser.txt", "r") as tempFile:
+        userReg = tempFile.readline()
+    personPath = f"Data/{userReg}/Images"
+
+    if not os.path.exists(personPath):
+        print('FOLDER CREATED:', personPath)
+        os.makedirs(personPath)
+
+    # We capture de photo
+    cap = cv2.VideoCapture(0)  # Choosing the camera
+    while (True):
+        ret, frame = cap.read()  # We read the video
+        cv2.imshow('Facial Register', frame)  # Show the video on screen
+        if cv2.waitKey(1) == 32:  # When we press 'space' key, we capture the last frame
+            break
+    print(personPath)
+    cv2.imwrite(personPath + "\\" + "face.jpg", frame)  # we save the last frame in the user folder as "rostro.jpg"
+    cap.release()  # We close the camera
+    cv2.destroyAllWindows()
+
+    def faceReg(img, lista_resultados):
+        data = pyplot.imread(img)
+        for i in range(len(lista_resultados)):
+            x1, y1, ancho, alto = lista_resultados[i]['box']
+            x2, y2 = x1 + ancho, y1 + alto
+            pyplot.subplot(1, len(lista_resultados), i + 1)
+            pyplot.axis('off')
+            cara_reg = data[y1:y2, x1:x2]
+            cara_reg = cv2.resize(cara_reg, (150, 200),
+                                  interpolation=cv2.INTER_CUBIC)  # Guardamos la imagen con un tamaño de 150x200
+            cv2.imwrite(userReg + ".jpg", cara_reg)
+            pyplot.imshow(data[y1:y2, x1:x2])
+        pyplot.show()
+
+    img = personPath + "\\" + "face.jpg"
+    pixels = pyplot.imread(img)
+    detector = MTCNN()
+    faces = detector.detect_faces(pixels)
+    faceReg(img, faces)
+
+
 # Create the main window
 window = tk.Tk()
 window.title("YouTube Audio Downloader")
-window.geometry("600x400")
+window.attributes("-fullscreen", True)
 
 backgroundImage = tk.PhotoImage(file="visuals/imágenesEspañol/7.png")
 
 backgroundLabel = tk.Label(window, image=backgroundImage)
 backgroundLabel.place(relwidth=1, relheight=1)
+
+window.bind("<Button-1>", on_click)
 
 # Set the initial size of the window based on the screen resolution
 screen_width = window.winfo_screenwidth()
@@ -149,13 +240,13 @@ status_label = tk.Label(frame, text="", bg="#121212", fg="#FF6C69", font=("Arial
 
 status_label.pack()
 
-song1 = tk.Label(window, text="1", bg="black", fg="#FF6C69", font=("Arial", 15))
+song1 = tk.Label(window, text="", bg="black", fg="#FF6C69", font=("Arial", 15))
 song1.place(x=370, y=300)
 
-song2 = tk.Label(window, text="2", bg="black", fg="#FF6C69", font=("Arial", 15))
+song2 = tk.Label(window, text="", bg="black", fg="#FF6C69", font=("Arial", 15))
 song2.place(x=370, y=400)
 
-song3 = tk.Label(window, text="3", bg="black", fg="#FF6C69", font=("Arial", 15))
+song3 = tk.Label(window, text="", bg="black", fg="#FF6C69", font=("Arial", 15))
 song3.place(x=370, y=500)
 
 q1 = tk.Entry(window, width=40, bg="#1f1f1f", font=("Arial", 10), fg="white")
